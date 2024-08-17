d13ea1b69436bd6c030e489d9161194bf1e71c54
Kelvin Ward <kelvinward1010@gmail.com>
2024-08-17 20:27:38 +0700

    update content nodejs

---
+++ src/modules/nodejs/data/index.ts
@@ -0,0 +1,19 @@
+import { IDataStatic } from "@/types/data";
+
+export const dataNodeJS: IDataStatic[] = [
+    {
+        key: "heading1",
+        data: [
+            "Node.js là một nền tảng runtime mã nguồn mở, đa nền tảng, được xây dựng trên V8 JavaScript Engine của Google Chrome12. Được phát triển bởi Ryan Dahl vào năm 2009, Node.js cho phép bạn chạy mã JavaScript bên ngoài trình duyệt, giúp xây dựng các ứng dụng web và máy chủ một cách hiệu quả.",
+            "Các đặc điểm nổi bật của Node.js:",
+            "1. Non-blocking I/O: Node.js sử dụng mô hình I/O không đồng bộ, giúp xử lý nhiều yêu cầu cùng lúc mà không bị chặn.",
+            "2. Single-threaded Event Loop: Node.js hoạt động trên một luồng đơn, sử dụng vòng lặp sự kiện để quản lý các yêu cầu.",
+            "3. NPM (Node Package Manager): Node.js đi kèm với NPM, một hệ thống quản lý gói mạnh mẽ, giúp dễ dàng cài đặt và quản lý các thư viện và module.",
+            "4. Cross-platform: Node.js có thể chạy trên nhiều hệ điều hành khác nhau như Windows, macOS, và Linux.",
+            "Ứng dụng của Node.js:",
+            "- Web Servers: Xây dựng các máy chủ web hiệu quả và có khả năng mở rộng.",
+            "- APIs: Tạo các API RESTful để giao tiếp giữa các ứng dụng.",
+            "- Real-time Applications: Phát triển các ứng dụng thời gian thực như chat, game trực tuyến.",
+        ],
+    },
+];
--- src/modules/nodejs/views/IntroduceNodejs.tsx
+++ src/modules/nodejs/views/IntroduceNodejs.tsx
@@ -1,12 +1,52 @@
 import { Typography } from "antd";
 import styles from "./Nodejs.module.scss";
+import { useFollowWidth } from "@/hooks/useFollowWidth";
+import { takeData } from "@/utils/data";
+import { dataNodeJS } from "../data";
+import { IDataStatic } from "@/types/data";
+import { QuickLinks } from "@/components/quicklinks/QuickLinks";
+import TextConfig from "@/components/textconfig/TextConfig";
 
 const { Title } = Typography;
 
+const list = [
+    {
+        key: "heading1",
+        lable: "1. Cơ bản về NodeJS.",
+    },
+];
+
 export function IntroduceNodejs() {
+    const { isBigger, isSmaller } = useFollowWidth(768);
+    const data1: IDataStatic = takeData("heading1", dataNodeJS);
+
     return (
         <div className={styles.container}>
-            <Title level={3}>This is IntroduceNodejs</Title>
+            {isSmaller && <QuickLinks list={list} />}
+            <div className={styles.content}>
+                <div className={styles.box}>
+                    <Title level={5} id="heading1">
+                        {list[0].lable}
+                    </Title>
+                    <TextConfig textdata={data1.data[0]} />
+                    <TextConfig
+                        classNameText="text-title"
+                        textdata={data1.data[1]}
+                    />
+                    <TextConfig textdata={data1.data[2]} />
+                    <TextConfig textdata={data1.data[3]} />
+                    <TextConfig textdata={data1.data[4]} />
+                    <TextConfig textdata={data1.data[5]} />
+                    <TextConfig
+                        classNameText="text-title"
+                        textdata={data1.data[6]}
+                    />
+                    <TextConfig textdata={data1.data[7]} />
+                    <TextConfig textdata={data1.data[8]} />
+                    <TextConfig textdata={data1.data[9]} />
+                </div>
+            </div>
+            {isBigger && <QuickLinks list={list} />}
         </div>
     );
 }
