65cf9257f4235d63e593626c80a437775bd38aad
Kelvin Ward <kelvinward1010@gmail.com>
2024-08-23 09:51:42 +0700

    update content database

--- src/modules/interviews/views/InterviewOther.tsx
+++ src/modules/interviews/views/InterviewOther.tsx
@@ -18,21 +18,24 @@ const list = [
         key: "heading2",
         lable: "2. Các cách xử lý bất đồng bộ trong JS(TS)",
     },
+    {
+        key: "heading3",
+        lable: "3. SQL và NoSQL",
+    },
 ];
 
 export function InterviewOther() {
     const { isBigger, isSmaller } = useFollowWidth(768);
     const data1: IDataStatic = takeData("heading1", dataInterviewOther);
     const data2: IDataStatic = takeData("heading2", dataInterviewOther);
+    const data3: IDataStatic = takeData("heading3", dataInterviewOther);
 
     return (
         <div className={styles.container}>
             {isSmaller && <QuickLinks list={list} />}
             <div className={styles.content}>
-                <div className={styles.box}>
-                    <Title level={5} id="heading1">
-                        {list[0].lable}
-                    </Title>
+                <div className={styles.box} id="heading1">
+                    <Title level={5}>{list[0].lable}</Title>
                     <TextConfig
                         classNameText="text-title"
                         textdata={data1.data[0]}
@@ -59,10 +62,8 @@ export function InterviewOther() {
                     <TextConfig textdata={data1.data[14]} />
                     <TextConfig textdata={data1.data[15]} />
                 </div>
-                <div className={styles.box}>
-                    <Title level={5} id="heading2">
-                        {list[1].lable}
-                    </Title>
+                <div className={styles.box} id="heading2">
+                    <Title level={5}>{list[1].lable}</Title>
                     <TextConfig
                         classNameText="text-title"
                         textdata={data2.data[0]}
@@ -76,6 +77,28 @@ export function InterviewOther() {
                     <TextConfig textdata={data2.data[7]} />
                     <TextConfig textdata={data2.data[8]} />
                     <TextConfig textdata={data2.data[9]} />
+                </div>
+                <div className={styles.box} id="heading2">
+                    <Title level={5}>{list[2].lable}</Title>
+                    <TextConfig
+                        classNameText="text-title"
+                        textdata={data3.data[0]}
+                    />
+                    <TextConfig textdata={data3.data[1]} />
+                    <TextConfig textdata={data3.data[2]} />
+                    <TextConfig textdata={data3.data[3]} />
+                    <TextConfig textdata={data3.data[4]} />
+                    <TextConfig textdata={data3.data[5]} />
+                    <TextConfig textdata={data3.data[6]} />
+                    <TextConfig textdata={data3.data[7]} />
+                    <TextConfig textdata={data3.data[8]} />
+                    <TextConfig textdata={data3.data[9]} />
+                    <TextConfig textdata={data3.data[10]} />
+                    <TextConfig textdata={data3.data[11]} />
+                    <TextConfig textdata={data3.data[12]} />
+                    <TextConfig textdata={data3.data[13]} />
+                    <TextConfig textdata={data3.data[14]} />
+                    <TextConfig textdata={data3.data[15]} />
                 </div>
             </div>
             {isBigger && <QuickLinks list={list} />}
